781dfc0a32d694331ccdc0c2ef65400f7beff984
gauravt1112 <33955859+gauravt1112@users.noreply.github.com>
2019-06-16 13:22:45 +0530

    index.php to index.html

--- contact-us.php
+++ contact-us.php
@@ -1,8 +1,66 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php include('includes/includes.html') ?>
-		   <?php include('includes/header1.html') ?>
+		  <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Sanathan</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+		 <link rel="shortcut icon" type="image/png" href="img/images/Favicon-01.png"/>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="css/timeline.css" rel="stylesheet">
+       
+        
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css" rel="stylesheet" media="screen">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+      
+       
+		    <div class="scroll-up1"> <a href="#top"><img src="img/back-to-top.png" style="width: 70%;"/></a></div>
+<div data-spy="" style="z-index: 99999;" class="nav-wrapper">
+<div class="logo" id="logo-id">
+		<a href="index.php"><img class="img-responsive" src="img/Untitled-1.png"></a>	
+
+	</div>
+	
+	<!-- Navigation start -->
+
+	<header class="header">
+
+		<nav class="navbar navbar-custom" role="navigation">
+
+			<div class="container">
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#custom-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<div class="collapse navbar-collapse" id="custom-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="who-we-are.php">WHO WE ARE</a></li>
+						<li><a href="what-we-do.php">WHAT WE DO</a></li>
+                        <li><a href="how-we-got-here.php">HOW WE GOT HERE</a></li>
+                        <li><a href="product.php">OUR PRODUCTS</a></li>
+						<li><a href="our-setup.php">OUR SET-UP</a></li>
+						<li><a href="our-reach.php">OUR REACH</a></li>
+						<li><a href="careers.php">CAREERS</a></li>
+						<li><a href="contact-us.php">CONTACT US</a></li>
+					</ul>
+				</div>
+
+			</div><!-- .container -->
+
+		</nav>
+
+	</header>
+	</div>
 		   <link rel="stylesheet" href="css/animate.css"/>
 		  
 	</head>
@@ -125,7 +183,39 @@
 	#	Script For Showing More Content				#
 	#################################################-->
 	
-	<?php include('includes/footer.html') ?>
+	<!-- footer starts here -->
+		<!--<footer class="site-footer">
+		<div class="footer-wrapper">
+		<div class="footer-nav">
+		<p>@Copyrights 2014 Sanathan Textiles Private Limited. All Rights Reserved |
+		Crafted by <a href="http://www.togglehead.in/">Togglehead</a></p>
+				
+		</div>
+		</div>
+		</footer>-->
+		
+		
+		<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1" style="text-align: center; width:100%; padding: 0; margin: 0;">
+		<div class="footer-nav">
+		<p>@Copyrights 2014 Sanathan Textiles Private Limited. All Rights Reserved |
+		Crafted by <a href="http://www.togglehead.in/">Togglehead</a></p>
+				
+		</div>
+		
+		</div>
+		</div>
+		</div>
+		<!--
+		<footer class="site-footer visible-xs">
+		<ul class="footer-nav">
+			<li>@Copyrights 2014 Sanathan Textiles Private Limited. All Rights Reserved</li>
+		<li>Crafted by <a href="http://www.togglehead.in/">Togglehead</a></li><br>
+		</ul>
+		</footer>-->
+	<!-- footer ends here -->	
+    
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
